49e7d24202b2f9947047b30351cb052d3803a324
JosephDuvvani <josephduvvani@gmail.com>
2024-07-02 19:30:34 +0200

    Create a function to call the math operator functions

--- scripts.js
+++ scripts.js
@@ -14,3 +14,23 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
+let firstNum = 0;
+let operator = '';
+let secondNum = 0;
+
+function operate(op, num1, num2){
+    switch(op){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;    
+    }
+}
